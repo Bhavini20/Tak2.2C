f75a138347123f7ff0160b7fc0d229eeb8f5f973
Bhavini20 <singhbhavini18@gmail.com>
2023-09-07 17:35:55 +0530

    more changes

--- MealHaus/src/router/index.js
+++ MealHaus/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import StartView from '../views/StartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- MealHaus/src/views/AboutView.vue
+++ MealHaus/src/views/AboutView.vue
@@ -8,7 +8,6 @@
 <script setup>
 import { ref } from "vue";
 
-
 </script>
 
 <style scoped>
